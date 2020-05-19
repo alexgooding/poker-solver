669b0de295a7224ba130828cb403ebb83c8f24e1
Alex Gooding <agooding@illumina.com>
2020-05-19 18:42:47 +0100

    create a card class

---
+++ Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerSolver
+{
+    class Card
+    {
+        public int Value { get; set; }
+        public string Suit { get; set; }
+    }
+}
