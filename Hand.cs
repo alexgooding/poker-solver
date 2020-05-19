ee72ec6e6b6257f9c45dda3cc40ed3f9da17fe88
Alex Gooding <alexxgooding@gmail.com>
2020-05-20 00:33:13 +0100

    make sure only the five highest cards in the flush are returned

--- Hand.cs
+++ Hand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PokerSolver
@@ -9,19 +10,30 @@ namespace PokerSolver
         private List<Card> hand;
         public Hand(List<Card> hand)
         {
-            this.hand = hand;
+            this.hand = hand.OrderByDescending(o => o.Value).ToList();
         }
         public Hand()
         {
             this.hand = new List<Card>();
         }
-        public void addCard(Card card)
+        public void addCard(Card card)     
         {
             hand.Add(card);
+            hand = hand.OrderByDescending(o => o.Value).ToList();
         }
         public int count()
         {
             return hand.Count;
+        }
+        public Hand getHighestNCards(int n)
+        {
+            Hand newHand = new Hand();
+            for (int i = 0; i < n; i++)
+            {
+                newHand.addCard(hand[i]);
+            }
+
+            return newHand;
         }
 
         public Hand isFlush()
@@ -56,19 +68,19 @@ namespace PokerSolver
             }
             if (clubCards.count() >= 5)
             {
-                return clubCards;
+                return clubCards.getHighestNCards(5);
             } 
             else if (diamondCards.count() >= 5)
             {
-                return diamondCards;
+                return diamondCards.getHighestNCards(5);
             }
             else if (heartCards.count() >= 5)
             {
-                return heartCards;
+                return heartCards.getHighestNCards(5);
             }
             else if (spadeCards.count() >= 5)
             {
-                return spadeCards;
+                return spadeCards.getHighestNCards(5);
             }
             else
             {
--- RunSolver.cs
+++ RunSolver.cs
@@ -12,8 +12,6 @@ namespace PokerSolver
 
             Hand flushCards = myCards.isFlush();
 
-            var v = 5;
-
         }
     }
 }
